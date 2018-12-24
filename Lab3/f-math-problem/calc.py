5f450cfa419efd141cab5fbbd7e737d1b31c7fbc
Trần Xuân Tùng <dieenemy1996@gmail.com>
2018-12-24 22:02:04 +0700

    initial commit

---
+++ Lab3/f-math-problem/calc.py
@@ -0,0 +1,14 @@
+def evaluate(a, b, c):
+    if c == "*":
+        d = a * b
+    elif c == "/":
+        d = a / b
+    elif c == "+":
+        d = a + b
+    elif c == "-":
+        d = a - b
+    return d
+
+
+# t = evaluate(5, 6)
+# print(t)
